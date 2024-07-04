3e2d87679a255bd9573808acf8b3bca6508c3d99
Ezequiel Almeida <ezequiellalmeida2003@gmail.com>
2024-07-04 18:57:53 -0300

    contaPoupanca

--- Menu.ts
+++ Menu.ts
@@ -1,47 +1,63 @@
 import readlinesync from 'readline-sync';
 import { colors } from './src/util/Color';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
+
+
 
 export function main() {
 
     let opcao: number;
 
+    const cp1 = new ContaPoupanca(123, 157, 2, "Ezequiel Almeida", 2000, 10)
+    const cp2 = new ContaPoupanca(123, 160, 2, "Eduardo Almeida", 150.000, 4)
+    cp1.visualizar();
+    cp2.visualizar();
+
     //Novas Instâncias da Classe Conta (Objetos)
-    const c1: Conta = new Conta(1, 1234, 1, 'Júlia Castro', 800000.00);
-    const c2: Conta = new Conta(2, 1234, 2, 'Marcella Sanches', 600000.00);
+    //const c1: Conta = new Conta(1, 1234, 1, 'Júlia Castro', 800000.00);
+    //const c2: Conta = new Conta(2, 1234, 2, 'Marcella Sanches', 600000.00);
 
     // Visualizando os dados da Conta c1
-    c1.visualizar();
+    //c1.visualizar();
 
     // Visualizando os dados da Conta c2
-    c2.visualizar();
+    //c2.visualizar();
 
     // Visualizando o Saldo da Conta c1
-    console.log(`\nO Saldo da conta 01 é: ${c1.saldo}`);
+    //console.log(`\nO Saldo da conta 01 é: ${c1.saldo}`);
 
     // Alterando o Saldo da Conta c2
-    c2.saldo = 900000.00;
+    //c2.saldo = 900000.00;
 
     // Visualizando o Saldo da Conta c2, depois de atualizar o valor
-    console.log(`\nO Saldo da conta 02 é: ${c2.saldo}`);
+   // console.log(`\nO Saldo da conta 02 é: ${c2.saldo}`);
 
     // Saque nas Contas
-    console.log(`\nSacar 100.00 Reais da Conta C1: ${c1.sacar(100)}`); // true
-    c1.visualizar();
+   // console.log(`\nSacar 100.00 Reais da Conta C1: ${c1.sacar(100)}`); // true
+    //c1.visualizar();
 
-    console.log(`\nSacar 1000000.00 Reais da Conta C2: ${c2.sacar(1000000)}`); // false
-    c2.visualizar();
+   // console.log(`\nSacar 1000000.00 Reais da Conta C2: ${c2.sacar(1000000)}`); // false
+   //c2.visualizar();
 
     // Depósito nas Contas
-    console.log(`\nDepositar 200000.00 Reais da Conta C1: `); 
-    c1.depositar(200000)
-    c1.visualizar();
+    //console.log(`\nDepositar 200000.00 Reais da Conta C1: `); 
+    //c1.depositar(200000)
+   // c1.visualizar();
 
-    console.log(`\nDepositar 300000.25 Reais da Conta C2: `); 
-    c2.depositar(300000.25)
-    c2.visualizar();
+   // console.log(`\nDepositar 300000.25 Reais da Conta C2: `); 
+   // c2.depositar(300000.25)
+   // c2.visualizar();
+
+//Novas Instâncias da Classe Conta Corrente (Objetos)
+const cc1: Conta = new ContaCorrente(1, 1234, 1, 'Amanda Magro', 100000.00, 100000);
+const cc2: Conta = new ContaCorrente(2, 1234, 1, 'Joao Silva', 1000.00, 100.00);
 
 
+
+cc1.visualizar();
+cc2.visualizar();
 
     while (true) {
 
@@ -137,7 +153,6 @@ export function main() {
 
 }
 
-/* Função com os dados da pessoa desenvolvedora */
 function sobre(): void {
 
 
---
+++ src/model/ContaCorrente.ts
@@ -0,0 +1,46 @@
+import { Conta } from "./Conta";
+
+export class ContaCorrente extends Conta{
+
+   private _limite: number;
+   
+
+	constructor(numero: number, agencia: number, tipo: number, titular: string,
+         saldo: number, limite: number) {
+        super(numero, agencia, tipo, titular, saldo);
+		this._limite = limite;
+
+	}
+   
+
+   
+	public get limite(): number {
+		return this._limite;
+	}
+
+    
+	public set limite(value: number) {
+		this._limite = value;
+	}
+
+    //Metodo sacar sobrescrito da Classe Conta
+
+    public sacar(valor: number): boolean{
+
+        if((this.saldo + this._limite) >= valor){
+            this.saldo = this.saldo - valor;
+        return true;
+
+        }
+
+        console.log("Saldo é insuficiente!")
+        return false;
+       
+
+    }
+    public visualizar(): void {
+        super.visualizar
+        console.log(`Limite: ${this._limite}`)
+
+    }
+}
